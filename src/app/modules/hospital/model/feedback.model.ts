bae8f648891b560a52ded872071494690c3c1906
DanaBrasanac <danabrasanac@gmail.com>
2022-12-16 20:49:10 +0100

    id dodati za selenium

--- src/app/modules/hospital/model/feedback.model.ts
+++ src/app/modules/hospital/model/feedback.model.ts
@@ -1,6 +1,7 @@
 export class Feedback {
   id: number = 0;
-  patientId: number=0;
+  patientName: string = '';
+  patientSurname: string = '';
   text: string = '';
   visibleToPublic: Boolean =false;
   approved: Boolean = false;
@@ -10,7 +11,8 @@ export class Feedback {
   public constructor(obj?: any) {
     if (obj) {
       this.id = obj.id;
-      this.patientId = obj.id;
+      this.patientName = obj.patientName;
+      this.patientSurname = obj.patientSurname;
       this.visibleToPublic = obj.visibleToPublic;
       this.approved = obj.approved;
       this.date = obj.date;
