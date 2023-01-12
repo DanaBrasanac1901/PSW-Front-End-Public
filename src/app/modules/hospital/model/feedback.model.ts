d18f9469e29ef54e05bb44ffe260c44df365adf8
tibbers-7 <dmitrovic.ra41.2019@uns.ac.rs>
2023-01-12 20:45:39 +0100

    fixed feedback

--- src/app/modules/hospital/model/feedback.model.ts
+++ src/app/modules/hospital/model/feedback.model.ts
@@ -2,6 +2,7 @@ export class Feedback {
   id: number = 0;
   patientName: string = '';
   patientSurname: string = '';
+  patientId:number=0;
   text: string = '';
   visibleToPublic: Boolean =false;
   approved: Boolean = false;
@@ -13,6 +14,7 @@ export class Feedback {
       this.id = obj.id;
       this.patientName = obj.patientName;
       this.patientSurname = obj.patientSurname;
+      this.patientId=obj.patientId;
       this.visibleToPublic = obj.visibleToPublic;
       this.approved = obj.approved;
       this.date = obj.date;
