90b959854e8c2440a6c941241bca1c418c602eae
tibbers-7 <dmitrovic.ra41.2019@uns.ac.rs>
2023-01-12 06:17:20 +0100

    popravka servisa

--- src/app/modules/hospital/services/patient.service.ts
+++ src/app/modules/hospital/services/patient.service.ts
@@ -15,34 +15,27 @@ export class PatientService {
   constructor(private http: HttpClient) { }
 
   getPatients(): Observable<Patient[]> {
-    return this.http.get<Patient[]>(this.apiHost + 'api/patients', { headers: this.headers });
+    return this.http.get<Patient[]>(this.apiHost + 'api/Patients', { headers: this.headers });
   }
 
   getPatient(id: number): Observable<Patient> {
-    return this.http.get<Patient>(this.apiHost + 'api/patients/' + id, { headers: this.headers });
-  }
-
-  deletePatient(id: any): Observable<any> {
-    return this.http.delete<any>(this.apiHost + 'api/patients/' + id, { headers: this.headers });
+    return this.http.get<Patient>(this.apiHost + 'api/Patients/' + id, { headers: this.headers });
   }
 
   registerPatient(user: any): Observable<any> {
     return this.http.post<any>(this.apiHost + 'api/Patients/register', user, { headers: this.headers });
   }
 
-  loginPatient(user: any): Observable<any> {
-    return this.http.post<any>(this.apiHost + 'api/patients/login', user, { headers: this.headers });
-  }
 
   getPatientByEmail(email: string): Observable<Patient> {
-    return this.http.get<Patient>(this.apiHost + 'api/patients/getPatientByEmail/' + email, { headers: this.headers });
+    return this.http.get<Patient>(this.apiHost + 'api/Patients/getByEmail/' + email, { headers: this.headers });
   }
 
   updatePatient(user: any): Observable<any> {
-    return this.http.put<any>(this.apiHost + 'api/patients/' + user.id, user, { headers: this.headers });
+    return this.http.put<any>(this.apiHost + 'api/Patients/' + user.id, user, { headers: this.headers });
   }
 
   getDoctors(): Observable<Doctor[]> {
-    return this.http.get<Doctor[]>(this.apiHost + 'api/patients/minimal-patients-doctor' , { headers: this.headers });
+    return this.http.get<Doctor[]>(this.apiHost + 'api/Patients/minimal-patients-doctor' , { headers: this.headers });
   }
 }
